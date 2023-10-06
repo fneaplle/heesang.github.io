7547875d7be7912a08cb09229196e66a0d9d8944
fneaplle <fneaplle@gmail.com>
2023-10-06 16:52:52 +0900

    prototype make

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const FrontTag = document.getElementsByClassName("FrontTag")
+
+for (var i = 0; i < FrontTag.length; i++) {
+    FrontTag.item(i).onmouseenter = (event) => {
+        event.target.style.transform = "scale(1.5)"; //1.2배 확대
+        event.target.style.zIndex = 1;
+        event.target.style.transition = "all 0.5s";// 속도
+    };
+
+    FrontTag.item(i).onmouseout = (event) => {
+        event.target.style.transform = "scale(1.0)"; //1.2배 확대
+        event.target.style.zIndex = 0;
+        event.target.style.transition = "all 0.5s";// 속도
+    };
+
+    FrontTag.item(i).onclick = (event) => {
+        location.href = "./html/heesang.html"
+    };
+}
